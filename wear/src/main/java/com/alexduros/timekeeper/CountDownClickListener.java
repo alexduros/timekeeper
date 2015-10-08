4e4ce825019e84a3645c12bddabd52a69262cc67
Alexandre Duros <alexandre.duros@gmail.com>
2015-10-08 12:11:14 +0200

    some logging and properly clean shot clock violation

--- wear/src/main/java/com/alexduros/timekeeper/CountDownClickListener.java
+++ wear/src/main/java/com/alexduros/timekeeper/CountDownClickListener.java
@@ -3,11 +3,13 @@ package com.alexduros.timekeeper;
 import android.content.Context;
 import android.os.CountDownTimer;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 class CountDownClickListener implements View.OnClickListener {
 
+    private static final String TAG = "CountDownClickListener";
     public static final int POSSESION_INTERVALL = 24000;
 
     public static final int SHOT_CLOCK_VIOLATION_DURATION = 1000;
@@ -22,25 +24,33 @@ class CountDownClickListener implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View v)
-    {
-        if (mCountDown != null) {
-            mCountDown.cancel();
-        }
+    public void onClick(View v) {
+        this.clearCountDown();
         mCountDown = new CountDownTimer(POSSESION_INTERVALL, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                mTextView.setText("" + millisUntilFinished / 1000);
+                String remainingTime = String.format("%d", millisUntilFinished / 1000);
+                Log.d(TAG, "set current remaing time " + remainingTime);
+                mTextView.setText(remainingTime);
             }
 
             public void onFinish() {
                 mTextView.setText("SCV");
+                Log.d(TAG, "shot clock violoation");
                 Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
                 v.vibrate(SHOT_CLOCK_VIOLATION_DURATION);
             }
         };
+        Log.d(TAG, "starting new countdown");
         mCountDown.start();
 
+    }
+
+    public void clearCountDown() {
+        if (mCountDown != null) {
+            Log.d(TAG, "canceling current countdown");
+            mCountDown.cancel();
+        }
     }
 
 }
--- wear/src/main/java/com/alexduros/timekeeper/TimekeeperActivity.java
+++ wear/src/main/java/com/alexduros/timekeeper/TimekeeperActivity.java
@@ -1,7 +1,7 @@
 package com.alexduros.timekeeper;
 
 import android.app.Activity;
-import android.content.Context;
+import android.util.Log;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
@@ -9,12 +9,15 @@ import android.widget.TextView;
 
 public class TimekeeperActivity extends Activity {
 
+    public final static String TAG = "TimekeeperActivity";
+
     TextView mTextView;
-    View.OnClickListener mClickListener;
+    CountDownClickListener mClickListener;
     Activity instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "creating activity");
         super.onCreate(savedInstanceState);
         instance = this;
         setContentView(R.layout.activity_timekeeper);
@@ -23,13 +26,34 @@ public class TimekeeperActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) findViewById(R.id.textView);
+
+                Log.d(TAG, "initialize text to 24 seconds");
                 mTextView.setText("24");
+
+                Log.d(TAG, "initialize clock listener");
                 mClickListener = new CountDownClickListener(mTextView, instance);
                 mTextView.setOnClickListener(mClickListener);
             }
         });
     }
-}
 
-;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "pausing activity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mClickListener.clearCountDown();
+        Log.d(TAG, "stopping activity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "destroying activity");
+    }
+};
 
