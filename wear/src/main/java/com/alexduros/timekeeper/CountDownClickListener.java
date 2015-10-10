e9b166b7427ee324d3d9f3d8abb56bfe72beebbb
Alexandre Duros <alexandre.duros@gmail.com>
2015-10-10 11:51:35 +0200

    added 8 seconds violation

--- wear/src/main/java/com/alexduros/timekeeper/CountDownClickListener.java
+++ wear/src/main/java/com/alexduros/timekeeper/CountDownClickListener.java
@@ -10,10 +10,13 @@ class CountDownClickListener implements View.OnClickListener {
 
     private static final String TAG = "CountDownClickListener";
 
-    public static final int POSSESION_INTERVALL = 24000;
     public static final boolean RUN_COUNTDOWN_AT_START = true;
 
-    public static final int SHOT_CLOCK_VIOLATION_DURATION = 1000;
+    public static final int EIGHT_SECONDS_VIOLATION_VIBRATE_DURATION = 100;
+    public static final int SHOT_CLOCK_VIOLATION_VIBRATE_DURATION = 500;
+
+    public static final int EIGHT_SECONDS_VIOLATION_DURATION  = 8000;
+    public static final int SHOT_CLOCK_VIOLATION_DURATION = 24000;
 
     TextView mTextView;
     CountDownTimerWithPause mCountDown;
@@ -30,9 +33,23 @@ class CountDownClickListener implements View.OnClickListener {
         if (mCountDown != null && mCountDown.isPaused()) {
             this.resumeCountDown();
         } else {
-            mCountDown = new CountDownTimerWithPause(POSSESION_INTERVALL, 1000, RUN_COUNTDOWN_AT_START) {
+            mCountDown = new CountDownTimerWithPause(SHOT_CLOCK_VIOLATION_DURATION, 1000, RUN_COUNTDOWN_AT_START) {
+
+                private void checkViolation(long millisUntilFinished) {
+                    int timeInPossession = SHOT_CLOCK_VIOLATION_DURATION - Math.round(millisUntilFinished / 1000) * 1000;
+                    Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+                    Log.d(TAG, String.format("time in possession %d", timeInPossession));
+                    switch (timeInPossession) {
+                        case EIGHT_SECONDS_VIOLATION_DURATION:
+                            v.vibrate(EIGHT_SECONDS_VIOLATION_VIBRATE_DURATION);
+                            break;
+                        default:
+                            break;
+                    }
+                }
 
                 public void onTick(long millisUntilFinished) {
+                    this.checkViolation(millisUntilFinished);
                     String remainingTime = String.format("%d", millisUntilFinished / 1000);
                     Log.d(TAG, "set current remaing time " + remainingTime);
                     mTextView.setText(remainingTime);
@@ -42,7 +59,7 @@ class CountDownClickListener implements View.OnClickListener {
                     mTextView.setText("SCV");
                     Log.d(TAG, "shot clock violoation");
                     Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
-                    v.vibrate(SHOT_CLOCK_VIOLATION_DURATION);
+                    v.vibrate(SHOT_CLOCK_VIOLATION_VIBRATE_DURATION);
                 }
             };
             Log.d(TAG, "starting new countdown");
